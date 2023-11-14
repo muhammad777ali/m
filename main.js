430b9ea613b554ee802d10a350016d08d03bd5a6
ali <ewqdewd@gmail.com>
2023-11-14 21:38:19 +0500

    birinci commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var login = prompt('Login kiriting')
+var pass = prompt('Parol kiriting')
+var userLoign = 'uzbek'
+var userPass = '12345678'
+if (login === userLoign && pass===userPass) {
+    console.log('Togri kiritngiz');
+} else{
+    console.log('Bunday login mavjud emas❗');
+}
+var nameTik = prompt('20+20=nechi boladi')
+if (nameTik === '40') {
+    console.log('javobingiz tori✅');
+} else{
+    console.log('javobingiz xato❌');
+}
+var nameTik = prompt('20+30=nechi boladi')
+if (nameTik === '50') {
+    console.log('javobingiz tori✅');
+} else{
+    console.log('javobingiz xato❌');
+}var nameTik = prompt('20+75=nechi boladi')
+if (nameTik === '95') {
+    console.log('javobingiz tori✅');
+} else{
+    console.log('javobingiz xato❌');
+}var nameTik = prompt('55-25=nechi boladi')
+if (nameTik === '30') {
+    console.log('javobingiz tori✅');
+} else{
+    console.log('javobingiz xato❌');
+}
